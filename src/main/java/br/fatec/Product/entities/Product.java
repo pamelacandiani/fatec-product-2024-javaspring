f6665f70f1ef69a2510ddd569615e8bdbf03319c
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-11-27 21:41:45 -0300

    Criacao das pastas, criando a entidade e hashCode Equals

---
+++ src/main/java/br/fatec/Product/entities/Product.java
@@ -0,0 +1,76 @@
+package br.fatec.Product.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product implements Serializable {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+
+    public String getCategory(){
+        return category;
+    }
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+ 
+    
+}
